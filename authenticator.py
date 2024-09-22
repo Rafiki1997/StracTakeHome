49db0aa115be071f98e54fd44b9231deee718fe1
Rahul Kumar <rahulxkumar13@gmail.com>
2024-09-22 16:56:48 -0500

    Added functionality for listing files

---
+++ authenticator.py
@@ -0,0 +1,35 @@
+import os
+import google.auth
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+
+# If modifying these SCOPES, delete the token.json file.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+def authenticate():
+    """Authenticate the user and return the Drive service instance."""
+    creds = None
+    # The file token.json stores the user's access and refresh tokens.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    # If there are no valid credentials, perform the OAuth 2.0 flow.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        # Save the credentials for future use.
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    # Return an authenticated Google Drive service instance.
+    return build('drive', 'v3', credentials=creds)
+
+
+service = authenticate()
