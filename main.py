75e1d19db2a7e453c0a2a35bedbec562094a4aa9
Rahul Kumar <rahulxkumar13@gmail.com>
2024-09-23 20:11:12 -0500

    Removed old operations.py with new version

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-import os
-import google.auth
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from google.auth.transport.requests import Request
-from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 from authenticator import authenticate
-from drive_operations import listFiles, selectFile, uploadFile
+from operations import authenticate, createGUI
 
-service = authenticate()
+def main():
+    service = authenticate()
+    if service:
+        createGUI(service)
 
-listFiles()
-file = selectFile()
-folder_id = input("Enter the Google Drive folder ID (Leave blank to upload to root directory)")
-
-if file:
-    uploadFile(file, folder_id if folder_id else None)
-else:
-    print("No file selected.")
+if __name__ == "__main__":
+    main()
