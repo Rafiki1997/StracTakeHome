415f6e2b7cf3eee3447bfbca66117e2d0a7931b0
Rahul Kumar <rahulxkumar13@gmail.com>
2024-09-23 20:17:07 -0500

    Updated comments in main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from operations import authenticate, createGUI
 def main():
     service = authenticate()
     if service:
+        # If authentication was successful, create GUI
         createGUI(service)
 
 if __name__ == "__main__":
